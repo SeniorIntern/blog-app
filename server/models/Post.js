492bb6ee75b9c0959161a93f5546c15afaedc1e9
Nikhil Thapa <tnikhil.official@gmail.com>
2023-03-06 23:59:55 +0545

    model: post model

--- server/models/Post.js
+++ server/models/Post.js
@@ -4,29 +4,44 @@ const mongoose = require('mongoose');
 const PostModel = mongoose.model(
   'posts',
   new mongoose.Schema({
-    title: {
-      type: String,
-      required: true,
-      unique: true,
-      minlength: 5,
-      maxlegth: 52,
+    blog: {
+      type: new Mongoose.Schema({
+        title: {
+          type: String,
+          required: true,
+          unique: true,
+          minlength: 5,
+          maxlegth: 52,
+        },
+        // todo- embedd user schema, categories
+        description: {
+          type: String,
+          required: true,
+          unique: true,
+          minlength: 20,
+          maxlegth: 800,
+        },
+        datePosted: {
+          type: Date,
+          default: Date.now,
+          required: true,
+        },
+        likeCount: {
+          type: Number,
+          default: 0,
+        },
+      }),
     },
-    // todo- embedd user schema, categories
-    description: {
-      type: String,
-      required: true,
-      unique: true,
-      minlength: 20,
-      maxlegth: 800,
-    },
-    datePosted: {
-      type: Date,
-      default: Date.now,
-      required: true,
-    },
-    likeCount: {
-      type: Number,
-      default: 0,
+    user: {
+      type: mongoose.Schema({
+        username: {
+          type: String,
+          required: true,
+          unique: true,
+          minlength: 5,
+          maxlegth: 22,
+        },
+      }),
     },
   })
 );
