001a9f225ba2f3386ec4a20125d6951029312d67
Nikhil Thapa <tnikhil.official@gmail.com>
2023-03-08 23:25:04 +0545

    component: new header component.

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main() {
+    return(
+        <main className="main">
+            <div className="testbox">
+                <h1>Hey...</h1>
+            </div>
+        </main>
+    )
+}
---
+++ client/src/components/Section.jsx
@@ -0,0 +1,7 @@
+export default function Section() {
+    return(
+        <section className="section">
+            
+        </section>
+    )
+}
