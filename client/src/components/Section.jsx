f3eb9b5529c647361f1f1839564e1602d847eb47
Nikhil Thapa <nikhilthapa.skybase@gmail.com>
2023-10-05 15:53:54 +0545

    Fix minor UI

--- client/src/components/Section.jsx
+++ client/src/components/Section.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import sponge from '../assets/img/sponge.jpeg';
-import { Link } from 'react-router-dom';
-import { apiUrl } from '../config.json';
-import '../styles/Section.css';
+import React, { useEffect, useState } from "react";
+import sponge from "../assets/img/sponge.jpeg";
+import { Link } from "react-router-dom";
+import { apiUrl } from "../config.json";
+import "../styles/Section.css";
 
 export default function Section({ name }) {
   console.log(name);
-  const apiEndpoint = apiUrl + 'users/';
+  const apiEndpoint = apiUrl + "users/";
 
   const [users, setUsers] = useState([]);
   useEffect(() => {
@@ -18,19 +18,23 @@ export default function Section({ name }) {
   }, []);
 
   return (
-    <section className='section'>
+    <section className="section">
       {name ? (
         <React.Fragment>
-          <span className='users__list'>
+          <span className="users__list">
             {users.map((user) => (
-              <figure className='figure' key={user._id}>
-                <Link to={`/user/${user.username}`} className='link'>
-                  <figcaption>{user.username}</figcaption>
-                  <img
-                    src={sponge}
-                    className='round__corners'
-                    alt={user.username}
-                  />
+              <figure className="figure" key={user._id}>
+                <Link to={`/user/${user.username}`} className="link">
+                  <span className="figure__container">
+                    <figcaption className="figure__caption">
+                      {user.username}
+                    </figcaption>
+                    <img
+                      src={sponge}
+                      className="figure__content"
+                      alt={user.username}
+                    />
+                  </span>
                 </Link>
               </figure>
             ))}
@@ -38,9 +42,11 @@ export default function Section({ name }) {
         </React.Fragment>
       ) : (
         <React.Fragment>
-          <div className='guest__message'>
-            <h2>Howdy User!!!</h2>
-            <h2>Join Us to Get Access to Amazing Features</h2>
+          <div className="message__container">
+            <div className="guest__message">
+              <h2>Howdy User!!!</h2>
+              <h2>Join Us to Get Access to Amazing Features</h2>
+            </div>
           </div>
         </React.Fragment>
       )}
